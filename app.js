bff2c3fa3f1f26ab22794c20a6cb8c10f8cd7f53
M Taufiq Permana S <35789663+taufiqpsumarna@users.noreply.github.com>
2022-06-05 17:30:20 +0700

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ app.get('/', function (req, res) {
   res.json({
     type: 'get',
     number: 1,
-    nama: 'Okyza Maherdy Prabowo',
-    npm: '33220025'
+    nama: 'M Taufiq Permana Sumarna',
+    npm: '2042802'
   });
 })
 
@@ -14,8 +14,8 @@ app.post('/', function (req, res) {
   res.json({
     type: 'post',
     number: 1,
-    nama: 'Okyza Maherdy Prabowo',
-    npm: '33220025'
+    nama: 'M Taufiq Permana Sumarna',
+    npm: '2042802'
   });
 })
 
